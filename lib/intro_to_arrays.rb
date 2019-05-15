152b3ac4bdff5c5f36f30040ebb0a667789badcc
Max Sun <max.sun@yale.edu>
2019-05-15 20:53:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,15 @@ end
 def array_with_two_elements
   return [1,2]
 end
+
+def first_element(array)
+  array.first
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array.last
+end
